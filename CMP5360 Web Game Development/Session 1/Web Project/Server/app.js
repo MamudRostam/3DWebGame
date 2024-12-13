ea50aac5212b5f11f8c7e0244b3a674147593780
MamudRostam <mamud.rostam@mail.bcu.ac.uk>
2024-12-13 16:08:47 +0000

    UpdatedServer
    Added 404 when page url is changed to the wrong link

--- CMP5360 Web Game Development/Session 1/Web Project/Server/app.js
+++ CMP5360 Web Game Development/Session 1/Web Project/Server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 var mysql = require('mysql');
@@ -20,18 +21,28 @@ con.connect(function(err) {
   });
 });
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.resolve('C:/Users/s23149743/GitHub/3DWebGame/CMP5360 Web Game Development/Session 1/Web Project')));
+
+// Serve HomePage.html as the default page
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(path.resolve('C:/Users/s23149743/GitHub/3DWebGame/CMP5360 Web Game Development/Session 1/Web Project/HomePage.html'));
+});
 
+// Additional routes
 app.get('/some', (req, res) => {
-    res.send('Change a word!')
-  })
+  res.send('Change a word!');
+});
 
-  app.get('/test', (req, res) => {
-    res.send('This is test page')
-  })
+app.get('/test', (req, res) => {
+  res.send('This is a test page');
+});
+
+// Catch-all route for 404 errors
+app.use((req, res) => {
+  res.status(404).sendFile(path.resolve('C:/Users/s23149743/GitHub/3DWebGame/CMP5360 Web Game Development/Session 1/Web Project/404.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
