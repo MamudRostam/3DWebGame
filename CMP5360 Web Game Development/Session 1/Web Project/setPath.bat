d34c97fb294576ab2e8e5d4f79b7c51958eccc18
MamudRostam <mamud.rostam@mail.bcu.ac.uk>
2024-12-15 21:46:54 +0000

    Updated

--- CMP5360 Web Game Development/Session 1/Web Project/setPath.bat
+++ CMP5360 Web Game Development/Session 1/Web Project/setPath.bat
@@ -1,3 +1,3 @@
-@cd C:\Users\s23149743\GitHub\3DWebGame\CMP5360 Web Game Development\Session 1\Web Project
-@set PATH=D:\nodejs;%PATH%
+@cd C:\Users\mamud\Documents\GitHub\3DWebGame\CMP5360 Web Game Development\Session 1\Web Project
+@set PATH=C:\nodejs%PATH%
 @cmd.exe /K
