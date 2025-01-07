ac884289103b2688387465a610c6584aa61eb277
MamudRostam <mamud.rostam@mail.bcu.ac.uk>
2025-01-07 11:51:43 +0000

    Update

--- CMP5360 Web Game Development/Session 1/Web Project/message.js
+++ CMP5360 Web Game Development/Session 1/Web Project/message.js
@@ -1,17 +1,17 @@
-// Variable in ES5
+
 var x = 1;
 var isClicked = true;
 
-// Variable in ES6
+
 let i = 0;
 const name = "Odyssey Shooters";
 
-// ES5
+
 function fname() {
     alert("This is a function");
 }
 
-// ES6
+
 const message = () => {
     const stringname = "Press Play to Begin";
     return stringname;
